9f201704040c302796709b85e1023bf163fb8c65
abhishek kumar majumdar <pk006604@gmail.com>
2025-06-08 23:35:00 +0530

    add bubble sort image

--- src/components/ExplainerCard.jsx
+++ src/components/ExplainerCard.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import bubble from '../Asset/image/bubble-sort.png'
 function ExplainerCard() {
+  const cardData = [
+    {
+      src: bubble,
+      alt: "3D Bubble Sort  Background",
+      title: "Bubble Sort Visualizer",
+      link: "/Algorithm/Sorting/bubble-sort"
+    },
+    {
+      src: "https://storage.googleapis.com/a1aa/image/83de498f-12ff-49a2-6221-d7332c754ab8.jpg",
+      alt: "Whiteboard Animation Toolkit with 4 Characters Holding Sign",
+      title: "Whiteboard animation toolkit",
+      link: "/Algorithm/Sorting/bubble-sort"
+    },
+    {
+      src: "https://storage.googleapis.com/a1aa/image/44c510f4-0a33-4748-3ef8-e5ddc2d313b2.jpg",
+      alt: "Trendy Typography Pack Blue Background with Text",
+      title: "Trendy typography pack",
+      link: "/Algorithm/Sorting/bubble-sort"
+    },
+    {
+      src: "https://storage.googleapis.com/a1aa/image/44c510f4-0a33-4748-3ef8-e5ddc2d313b2.jpg",
+      alt: "Trendy Typography Pack Blue Background with Text",
+      title: "Trendy typography pack",
+      link: "/Algorithm/Sorting/bubble-sort"
+    },
+  ];
+
   return (
     <div className="bg-[#e9fbff] py-10 px-4">
       <div className="max-w-7xl mx-auto">
@@ -10,31 +38,11 @@ function ExplainerCard() {
 
         <div className="relative mt-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-6">
-            {[
-              {
-                src: "https://storage.googleapis.com/a1aa/image/09c4563a-c80d-4e2d-568f-bcb24bdc4196.jpg",
-                alt: "3D Animations Group of People and Dog on Blue Background",
-                title: "Factory of 3D Animations",
-              },
-              {
-                src: "https://storage.googleapis.com/a1aa/image/83de498f-12ff-49a2-6221-d7332c754ab8.jpg",
-                alt: "Whiteboard Animation Toolkit with 4 Characters Holding Sign",
-                title: "Whiteboard animation toolkit",
-              },
-              {
-                src: "https://storage.googleapis.com/a1aa/image/44c510f4-0a33-4748-3ef8-e5ddc2d313b2.jpg",
-                alt: "Trendy Typography Pack Blue Background with Text",
-                title: "Trendy typography pack",
-              },
-              {
-                src: "https://storage.googleapis.com/a1aa/image/44c510f4-0a33-4748-3ef8-e5ddc2d313b2.jpg",
-                alt: "Trendy Typography Pack Blue Background with Text",
-                title: "Trendy typography pack",
-              },
-             
-            ].map((item, i) => (
-              <div
+            {cardData.map((item, i) => (
+              <Link
+                to={item.link}
                 key={i}
+                aria-label={`Go to ${item.title}`}
                 className={`bg-white rounded-xl overflow-hidden shadow-md cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-xl ${
                   i >= 4 ? 'col-span-1 sm:col-span-2 md:col-span-1' : ''
                 }`}
@@ -51,7 +59,7 @@ function ExplainerCard() {
                     {item.title}
                   </div>
                 )}
-              </div>
+              </Link>
             ))}
           </div>
         </div>
